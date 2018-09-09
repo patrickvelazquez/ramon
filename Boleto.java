3aa71db3b044db24ff8ac81d0168673411c47be3
patrickvelazquez <43104636+patrickvelazquez@users.noreply.github.com>
2018-09-09 17:11:57 -0600

    nuevo proyecto

---
+++ Boleto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inicial;
+
+/**
+ *
+ * @author patrickMac
+ */
+public class Boleto { 
+    private int idBoleto;
+    private String destino;
+    private int tipo;
+    private float precio;
+    
+}
